59da1e0e811288578cc30a6ff1e33863f0bd3c8d
xiajingzero <295850893.com>
2018-09-05 11:36:53 +0800

    hello world

---
+++ src/main/java/com/xiajingzero/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xiajingzero.hello;
+
+/**
+ * Created by XiaJing on 2018/9/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
